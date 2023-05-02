b8b010462b7a6ace9c8206a3ac81daef9dce5de1
TJ <teejaynam@gmail.com>
2023-05-02 16:44:54 -0400

    isOdd activity, first commit

---
+++ odd.js
@@ -0,0 +1,8 @@
+const isOdd = function(num){
+  return num % 2 === 1;
+}
+
+console.log("9 is odd: " + isOdd(9));
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("967 is odd: " + isOdd(967));
