73c29ff97a6018797b682472970189de96ec3985
TJ <teejaynam@gmail.com>
2023-05-02 16:28:59 -0400

    function example. second commit

--- hello.js
+++ hello.js
@@ -2,6 +2,20 @@ const sayHello = function(name){
   console.log("Hello, world and " + name)
 }
 
+/*
 sayHello("Jeffery");
 sayHello("Johnson");
 sayHello("Jeremy");
+*/
+
+const sayHelloToConsole = function(name){
+  console.log("HELLO " + name);
+}
+
+const returnSayHello = function(name){
+  return "hello " + name;
+}
+
+sayHelloToConsole("Connor");
+const greeting = returnSayHello("Ryan");
+console.log(greeting);
