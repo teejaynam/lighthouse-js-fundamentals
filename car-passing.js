70e2f35457cd012e1187b34eda25fec363a97381
TJ <teejaynam@gmail.com>
2023-05-04 23:09:26 -0400

    cars passing assesment first commit

---
+++ car-passing.js
@@ -0,0 +1,30 @@
+const carPassing = function (cars, speed) {
+  var newCars = 
+    {
+      time: Date.now(),
+      speed: speed
+    };
+  cars.push(newCars);
+  //return cars; in test
+}
+
+const cars = [
+  {
+    time: 1568329654807,
+    speed: 40,
+  },
+  {
+    time: 1568329821632,
+    speed: 42,
+  },
+  {
+    time: 1568331115463,
+    speed: 35
+  }
+]
+
+const speed = 38
+
+carPassing(cars, speed)
+
+console.log(cars);
